0c8dd4b968a4bd35db64ebfc0cdfdd828aef65ae
O1sims <sims.owen@gmail.com>
2019-03-12 21:36:32 +0000

    R script to map all border crossings

---
+++ research/coordinate-mapping/map_border_crossings.R
@@ -0,0 +1,90 @@
+library(jsonlite)
+library(magrittr)
+
+
+generate_shapefile_map <- function() {
+  spdf <- getwd() %>% 
+    paste0("/coordinate-mapping/data/map/parliamentaries/NI-parliamentary-boundaries.shp") %>%
+    maptools::readShapePoly()
+  
+  spdf@data$id <- rownames(spdf@data)
+  spdf.points <- ggplot2::fortify(spdf, region = "id")
+  heatmap.data <- dplyr::inner_join(
+    spdf.points, 
+    spdf@data, 
+    by = "id")
+  
+  heatmap.data$COUNTYNAME %<>% 
+    as.vector()
+  
+  shapefileMap <- ggplot(
+    data = heatmap.data) + 
+    geom_polygon(
+      colour = "white",
+      fill = "grey",
+      size = 0.5, 
+      aes(
+        x = long, 
+        y = lat, 
+        group = group))
+  
+  return(shapefileMap)
+}
+
+
+borderCrossingPoints <- getwd() %>%
+  paste0("/coordinate-mapping/data/border-points/borderCrossingPoints2018.geojson") %>%
+  jsonlite::fromJSON()
+
+borderData <- cbind(
+  borderCrossingPoints$features$properties, 
+  borderCrossingPoints$features$geometry)
+
+lat <- long <- mainClass <- c()
+for (i in 1:(borderData %>% nrow())) {
+  lat %<>% append(borderData$coordinates[i][[1]][1])
+  long %<>% append(borderData$coordinates[i][[1]][2])
+  splitRoadClass <- borderData$RoadClass[i] %>% 
+    strsplit(split = " - ")
+  splitRoadClass <- splitRoadClass[[1]][1]
+  splitRoadClass %<>% strsplit(split = "- ")
+  mainClass %<>% append(splitRoadClass[[1]][1])
+}
+
+borderData$lat <- lat
+borderData$long <- long
+borderData$mainClass <- mainClass
+
+shapefileMap <- generate_shapefile_map()
+
+shapefileMap +
+  geom_point(
+    data = borderData,
+    aes(
+      x = lat, 
+      y = long,
+      colour = mainClass,
+      size = 2),
+    alpha = 0.6) + 
+  labs(
+    color = "Road class") +
+  ylab("") + 
+  xlab("") +
+  coord_cartesian(
+    xlim = c(-7.75, -7.25),
+    ylim = c(54.6, 55.1)) + 
+  scale_size(guide = 'none') +
+  theme_minimal() +
+  theme(
+    panel.border = element_blank(), 
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    axis.text = element_blank(),
+    panel.background = element_blank(),
+    axis.ticks = element_blank(),
+    legend.position = "right") +
+  annotate(
+    geom = "text", 
+    x = -5.3, 
+    y = 55.2, 
+    label = paste0(max(borderData$Id), " border crossing points"))
